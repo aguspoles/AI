bb14fca22b26305c9c06fabe041562339760004b
Agustin Polleschi <agustin.polleschi@globant.com>
2019-08-22 17:22:46 -0300

    updating state machine

--- IA/Assets/Scripts/PlayerBehaviour.cs
+++ IA/Assets/Scripts/PlayerBehaviour.cs
@@ -4,103 +4,119 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    enum STATES {
-        IDLE = 0,
-        GO_TO_MINE = 1,
-        MINE = 2,
-        RETURN_BASE = 3,
-        LEAVE_GOLD = 4
-    }
-    enum EVENTS {
-        MINE_EXIST = 0,
-        MINE_NOT_EXIST = 1,
-        ARRIVE_TO_MINE = 2,
-        POCKETS_FULL = 3,
-        ARRIVE_TO_HOME = 4,
-        LEAVE_GOLD = 5
-    }
-
-    FSM playerStateMachine = new FSM (5, 6);
-
     public float goldPocketSize = 50;
     public float goldInPocket = 0;
-
     public float speed = 5;
     public float miningStep = 1;
-    private GameObject home;
+
+    private GameObject m_home;
+    private GameObject[] m_mines;
+    private CEnemyStateMachine m_playerStateMachine;
 
     // Use this for initialization
     void Start () {
-        home = GameObject.Find ("Base");
-        for (uint i = 0; i < playerStateMachine.fsm.GetLength (0); i++) {
-            for (uint j = 0; j < playerStateMachine.fsm.GetLength (1); j++) {
-                playerStateMachine.fsm[i, j] = -1;
-            }
-        }
-        playerStateMachine.SetRelation ((int) STATES.IDLE, (int) EVENTS.MINE_EXIST, (int) STATES.GO_TO_MINE);
-        playerStateMachine.SetRelation ((int) STATES.IDLE, (int) EVENTS.MINE_NOT_EXIST, (int) STATES.IDLE);
-        playerStateMachine.SetRelation ((int) STATES.GO_TO_MINE, (int) EVENTS.ARRIVE_TO_MINE, (int) STATES.MINE);
-        playerStateMachine.SetRelation ((int) STATES.MINE, (int) EVENTS.POCKETS_FULL, (int) STATES.RETURN_BASE);
-        playerStateMachine.SetRelation ((int) STATES.RETURN_BASE, (int) EVENTS.ARRIVE_TO_HOME, (int) STATES.LEAVE_GOLD);
-        playerStateMachine.SetRelation ((int) STATES.LEAVE_GOLD, (int) EVENTS.LEAVE_GOLD, (int) STATES.IDLE);
-        playerStateMachine.SetEvent ((int) EVENTS.MINE_NOT_EXIST);
+        m_home = GameObject.Find ("Base");
+        m_mines = GameObject.FindGameObjectsWithTag("Mine");
+        m_playerStateMachine = new CEnemyStateMachine();
     }
 
     // Update is called once per frame
     void Update () {
-        List<GameObject> mines = MinesWithGold ();
-
-        if (mines.Count > 0) {
-            playerStateMachine.SetEvent ((int) EVENTS.MINE_EXIST);
-        } else {
-            playerStateMachine.SetEvent ((int) EVENTS.MINE_NOT_EXIST);
-        }
-
-        if (playerStateMachine.GetState () == (int) STATES.IDLE) {
-            transform.RotateAround (transform.position, Vector3.up, 90 * Time.deltaTime);
-        }
-
-        else if (playerStateMachine.GetState () == (int) STATES.GO_TO_MINE) {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards (transform.position, mines[0].transform.position, step);
-            if (transform.position == mines[0].transform.position)
-                playerStateMachine.SetEvent ((int) EVENTS.ARRIVE_TO_MINE);
-        } 
-
-        else if (playerStateMachine.GetState () == (int) STATES.MINE) {
-            float amountMined = miningStep * Time.deltaTime;
-            goldInPocket += amountMined;
-            if (goldInPocket >= goldPocketSize) {
-                goldInPocket = goldPocketSize;
-                mines[0].GetComponent<GoldMine> ().goldLeft -= goldPocketSize;
-                playerStateMachine.SetEvent ((int) EVENTS.POCKETS_FULL);
-            }
-        } 
-
-        else if (playerStateMachine.GetState () == (int) STATES.RETURN_BASE) {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards (transform.position, home.transform.position, step);
-            if (transform.position == home.transform.position) {
-                playerStateMachine.SetEvent ((int) EVENTS.ARRIVE_TO_HOME);
-            }
-        }
-         
-        else if (playerStateMachine.GetState () == (int) STATES.LEAVE_GOLD) {
-            goldInPocket = 0;
-            playerStateMachine.SetEvent ((int) EVENTS.LEAVE_GOLD);
-        }
-
-}
-
-private List<GameObject> MinesWithGold () {
-    List<GameObject> res = new List<GameObject> ();
-    GameObject[] mines = GameObject.FindGameObjectsWithTag ("Mine");
-    foreach (GameObject mine in mines) {
-        GoldMine goldMine = mine.GetComponent<GoldMine> ();
-        if (goldMine.goldLeft > 0) {
-            res.Add (mine);
+       
+        switch (m_playerStateMachine.GetState())
+        {
+            case (int)CEnemyStateMachine.EState.IDLE:
+                Idle();
+                break;
+            case (int)CEnemyStateMachine.EState.GO_TO_MINE:
+                GoToMine();
+                break;
+            case (int)CEnemyStateMachine.EState.MINE:
+                Mine();
+                break;
+            case (int)CEnemyStateMachine.EState.RETURN_TO_BASE:
+                ReturnToBase();
+                break;
+            case (int)CEnemyStateMachine.EState.LEAVE_GOLD:
+                LeaveGold();
+                break;
+            default:
+                break;
         }
     }
-    return res;
-}
+
+    private GameObject MineWithGold()
+    {
+        foreach (GameObject mine in m_mines)
+        {
+            GoldMine goldMine = mine.GetComponent<GoldMine>();
+            if (goldMine.goldLeft > 0)
+            {
+                return mine;
+            }
+        }
+        return null;
+    }
+
+    private void Idle()
+    {
+        transform.RotateAround(transform.position, Vector3.up, 90 * Time.deltaTime);
+        if (MineWithGold())
+        {
+            m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_MINE_EXIST);
+        }
+        else
+        {
+            m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_MINE_NOT_EXIST);
+        }
+    }
+
+    private void GoToMine()
+    {
+        GameObject mine = MineWithGold();
+        if (mine)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, mine.transform.position, step);
+            if (transform.position == mine.transform.position)
+                m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_ARRIVE_TO_MINE);
+        }
+        Debug.Log("Going to mine...");
+    }
+
+    private void Mine()
+    {
+        GameObject mine = MineWithGold();
+        if (mine)
+        {
+            float amountMined = miningStep * Time.deltaTime;
+            goldInPocket += amountMined;
+            if (goldInPocket >= goldPocketSize)
+            {
+                goldInPocket = goldPocketSize;
+                mine.GetComponent<GoldMine>().goldLeft -= goldPocketSize;
+                m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_POCKETS_FULL);
+            }
+        }
+        Debug.Log("Mining...");
+    }
+
+    private void ReturnToBase()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, m_home.transform.position, step);
+        if (transform.position == m_home.transform.position)
+        {
+            m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_ARRIVE_TO_HOME);
+        }
+        Debug.Log("Returning to base...");
+    }
+
+    private void LeaveGold()
+    {
+        goldInPocket = 0;
+        m_playerStateMachine.SetEvent((int)CEnemyStateMachine.EEvent.ON_LEAVE_GOLD);
+        Debug.Log("Leaving gold...");
+    }
+
 }
